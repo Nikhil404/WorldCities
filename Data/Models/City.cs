602358dac03e3843e2c74f89f6da52e2e45c8fe3
Nikhil404 <nikhilbanuri4@gmail.com>
2021-02-18 14:02:38 +0530

    Code First Models are added with properties

---
+++ Data/Models/City.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class City
+    {
+        #region Constructor
+        public City()
+        {
+                
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The Unique id and primary key for city
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// City Name(in UTF8 Format)
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///  City Name(in ASCII Format)
+        /// </summary>
+        public string Name_ASCII { get; set; }
+
+        /// <summary>
+        /// City Latitude
+        /// </summary>
+        public decimal Lat { get; set; }
+
+        /// <summary>
+        /// City Logitude
+        /// </summary>
+        public decimal Lon { get; set; }
+
+        /// <summary>
+        /// CountryId(foreign Key)
+        /// </summary>
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        /// <summary>
+        /// The Country related to city
+        /// </summary>
+        public virtual Country Country { get; set; }
+        #endregion
+
+    }
+}
