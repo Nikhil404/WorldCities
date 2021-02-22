f69ea71449fe9208ff69a7baedae268770f89e26
Nikhil404 <nikhilbanuri4@gmail.com>
2021-02-22 10:20:33 +0530

    cities data displayed

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,30 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { NavMenuComponent } from "./nav-menu/nav-menu.component";
+import { HomeComponent } from "./home/home.component";
+import { CitiesComponent } from "./cities/cities.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
-    
+    HomeComponent,
+    CitiesComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "cities", component: CitiesComponent }
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- ClientApp/src/app/cities/cities.component.ts
+++ ClientApp/src/app/cities/cities.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { City } from "./city";
 
 @Component({
-  selector: 'app-cities',
-  templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.css']
+  selector: "app-cities",
+  templateUrl: "./cities.component.html",
+  styleUrls: ["./cities.component.css"]
 })
-export class CitiesComponent implements OnInit {
+export class CitiesComponent {
+  public cities: City[];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    @Inject("BASE_URL") private baseUrl: string
+  ) {}
 
   ngOnInit() {
+    this.http.get<City[]>(this.baseUrl + "api/Cities").subscribe(
+      result => {
+        this.cities = result;
+      },
+      error => console.error(error)
+    );
   }
-
 }
