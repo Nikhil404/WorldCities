689e998e534fd0c569f8144f880307417346b169
Nikhil404 <nikhilbanuri4@gmail.com>
2021-02-22 12:37:18 +0530

    pagination and some fun

--- ClientApp/src/app/angular-material.module.ts
+++ ClientApp/src/app/angular-material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
 
 @NgModule({
-  imports: [MatTableModule],
-  exports: [MatTableModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+  exports: [MatTableModule, MatPaginatorModule, MatSortModule]
 })
 export class AngularMaterialModule {}
