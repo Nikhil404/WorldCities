43e0bd777e86017fbbcf8a4c100f6a857df75d32
Nikhil404 <nikhilbanuri4@gmail.com>
2021-02-23 18:41:36 +0530

    service for country

--- ClientApp/src/app/countries/countries.component.ts
+++ ClientApp/src/app/countries/countries.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject, ViewChild } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
 import { MatTableDataSource } from "@angular/material/table";
 import { MatPaginator, PageEvent } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 
 import { Country } from "./country";
+import { CountryService } from "./country.service";
+import { ApiResult } from "../base.service";
 
 @Component({
   selector: "app-countries",
@@ -21,28 +22,25 @@ export class CountriesComponent {
   ];
   public countries: MatTableDataSource<Country>;
 
-  defaultPageIndex: number = 0;
-  defaultPageSize: number = 10;
-  public defaultSortColumn: string = "name";
-  public defaultSortOrder: string = "asc";
+  defaultPageIndex = 0;
+  defaultPageSize = 10;
+  public defaultSortColumn = "name";
+  public defaultSortOrder = "asc";
 
-  defaultFilterColumn: string = "name";
+  defaultFilterColumn = "name";
   filterQuery: string = null;
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
 
-  constructor(
-    private http: HttpClient,
-    @Inject("BASE_URL") private baseUrl: string
-  ) {}
+  constructor(private countryService: CountryService) {}
 
   ngOnInit() {
     this.loadData(null);
   }
 
   loadData(query: string = null) {
-    var pageEvent = new PageEvent();
+    const pageEvent = new PageEvent();
     pageEvent.pageIndex = this.defaultPageIndex;
     pageEvent.pageSize = this.defaultPageSize;
     if (query) {
@@ -52,24 +50,23 @@ export class CountriesComponent {
   }
 
   getData(event: PageEvent) {
-    var url = this.baseUrl + "api/Countries";
-    var params = new HttpParams()
-      .set("pageIndex", event.pageIndex.toString())
-      .set("pageSize", event.pageSize.toString())
-      .set("sortColumn", this.sort ? this.sort.active : this.defaultSortColumn)
-      .set(
-        "sortOrder",
-        this.sort ? this.sort.direction : this.defaultSortOrder
-      );
+    var sortColumn = this.sort ? this.sort.active : this.defaultSortColumn;
 
-    if (this.filterQuery) {
-      params = params
-        .set("filterColumn", this.defaultFilterColumn)
-        .set("filterQuery", this.filterQuery);
-    }
+    var sortOrder = this.sort ? this.sort.direction : this.defaultSortOrder;
 
-    this.http
-      .get<any>(url, { params })
+    var filterColumn = this.filterQuery ? this.defaultFilterColumn : null;
+
+    var filterQuery = this.filterQuery ? this.filterQuery : null;
+
+    this.countryService
+      .getData<ApiResult<Country>>(
+        event.pageIndex,
+        event.pageSize,
+        sortColumn,
+        sortOrder,
+        filterColumn,
+        filterQuery
+      )
       .subscribe(
         result => {
           this.paginator.length = result.totalCount;
